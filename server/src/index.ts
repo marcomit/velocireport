61b1e4d98048038d77a96ef287f5d0a6977ef785
FrancescoVenanti <francesco.venanti@gmail.com>
2024-10-05 09:55:39 +0200

    cors policies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
-import express from 'express';
-import fs from 'fs/promises';
+import express from "express";
+import fs from "fs/promises";
+import cors from "cors";
 import path from "path";
 import puppeteer from "puppeteer";
 import {
@@ -14,6 +15,13 @@ import { default as pdf } from "./router/pdf";
 import { default as templates } from "./router/templates";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3001", // Allow requests from this origin
+    methods: ["GET", "POST"], // Allow only GET and POST requests
+    credentials: true, // Include credentials in CORS requests
+  })
+);
 const PORT = process.env.PORT || 80;
 
 app.use(express.json());
