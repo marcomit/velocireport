38caa9c5f4887ec612127b603a73be6591390a0f
FrancescoVenanti <francesco.venant@gmail.com>
2024-10-08 15:43:43 +0200

    monaco custom theme

--- client/src/components/text-editor.tsx
+++ client/src/components/text-editor.tsx
@@ -3,6 +3,7 @@
 import useTabs from "@/stores/tabs";
 import Editor, { OnChange } from "@monaco-editor/react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const languages = new Map<string, string>([
   ["ts", "typescript"],
@@ -14,6 +15,37 @@ const languages = new Map<string, string>([
 const TextEditor = () => {
   const { selected, updateContent } = useTabs();
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false); // To check if the theme is mounted
+
+  const defineCustomThemes = (monaco: any) => {
+    // Define the dark theme
+    monaco.editor.defineTheme("custom-dark", {
+      base: "vs-dark", // Can be 'vs', 'vs-dark', or 'hc-black'
+      inherit: true, // Inherit from the base theme
+      rules: [],
+      colors: {
+        "editor.background": "#0A0A0A", // Editor background color
+        "editor.lineHighlightBackground": "#474747", // Line highlight color
+        // Add more custom colors here
+      },
+    });
+
+    // Define the light theme
+    monaco.editor.defineTheme("custom-light", {
+      base: "vs", // 'vs' is the default light theme
+      inherit: true,
+      rules: [],
+      colors: {
+        "editor.background": "#eeeeee", // Editor background color
+        "editor.lineHighlightBackground": "#bebebe", // Line highlight color
+        // Add more custom colors here
+      },
+    });
+  };
+
+  const handleEditorWillMount = (monaco: any) => {
+    defineCustomThemes(monaco);
+  };
 
   const handleChange: OnChange = (value) => {
     if (selected) {
@@ -28,13 +60,20 @@ const TextEditor = () => {
     return "";
   };
 
+  useEffect(() => {
+    setMounted(true); // Ensure the component is fully mounted to apply the theme
+  }, []);
+
+  if (!mounted) return null; // Prevent the editor from rendering until fully mounted
+
   return (
     <Editor
       defaultLanguage="javascript"
       language={getLanguage()}
-      theme={theme === "light" ? "vs-dark" : "vs-dark"}
+      theme={theme === "light" ? "custom-light" : "custom-dark"}
       onChange={handleChange}
       value={selected ? (selected.content as string) : ""}
+      beforeMount={handleEditorWillMount} // Define custom themes before the editor mounts
     />
   );
 };
