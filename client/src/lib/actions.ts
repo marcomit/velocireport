7e22a74b5168c87dd3d8b58169b9dc7582cd3d55
FrancescoVenanti <francesco.venant@gmail.com>
2024-10-09 11:36:47 +0200

    default=true

--- client/src/lib/actions.ts
+++ client/src/lib/actions.ts
@@ -6,7 +6,7 @@ const createTemplate = async (e: FormData) => {
   console.log(e.get("name"));
   const response = await axios.post(
     `http://localhost:8000/templates/${e.get("name")}?${
-      e.get("default") && "classic"
+      e.get("default") && "default=true"
     }`
   );
   console.log(response.data);
