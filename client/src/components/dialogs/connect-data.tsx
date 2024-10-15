9e0b53aad0d8c4651d94ff35b0da42ce5b8bae69
FrancescoVenanti <francesco.venant@gmail.com>
2024-10-15 15:27:24 +0200

    connect data logic handled, TODO post

--- client/src/components/dialogs/connect-data.tsx
+++ client/src/components/dialogs/connect-data.tsx
@@ -16,7 +16,31 @@ import { DirectoryTree } from "@/types/directory";
 import { Textarea } from "../ui/textarea";
 
 const ConnectDataDialog = ({ selected }: { selected: DirectoryTree }) => {
+  const [type, setType] = useState<"rawdata" | "file" | "import" | "fetch">(
+    "rawdata"
+  );
   const [selectedFile, setSelectedFile] = useState<DirectoryTree | null>(null);
+  const [name, setName] = useState<string>(selected.name + "Data");
+  const [rawData, setRawData] = useState<string>("");
+  const [importedFile, setImportedFile] = useState<File | null>(null);
+  const [fetchData, setFetchData] = useState({
+    url: "",
+    method: "",
+    headers: "",
+    body: "",
+  });
+
+  function handleSubmit() {
+    //TODO: send data to server
+    console.log({
+      name,
+      type,
+      rawData,
+      selectedFile,
+      importedFile,
+      fetchData,
+    });
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -34,21 +58,37 @@ const ConnectDataDialog = ({ selected }: { selected: DirectoryTree }) => {
         </DialogHeader>
         <div className="flex items-center gap-4">
           <Label htmlFor="name">Name</Label>
-          <Input id="name" defaultValue={selected.name + "Data"} />
+          <Input
+            id="name"
+            defaultValue={selected.name + "Data"}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <Tabs
           defaultValue="rawdata"
           className="flex flex-col items-center justify-center"
         >
           <TabsList>
-            <TabsTrigger value="rawdata">Raw Data</TabsTrigger>
-            <TabsTrigger value="file">Select existing file</TabsTrigger>
-            <TabsTrigger value="import">Import file</TabsTrigger>
-            <TabsTrigger value="fetch">Api Endpoint</TabsTrigger>
+            <TabsTrigger value="rawdata" onClick={(e) => setType("rawdata")}>
+              Raw Data
+            </TabsTrigger>
+            <TabsTrigger value="file" onClick={(e) => setType("file")}>
+              Select existing file
+            </TabsTrigger>
+            <TabsTrigger value="import" onClick={(e) => setType("import")}>
+              Import file
+            </TabsTrigger>
+            <TabsTrigger value="fetch" onClick={(e) => setType("fetch")}>
+              Api Endpoint
+            </TabsTrigger>
           </TabsList>
           <TabsContent value="rawdata">
             paste your raw data here.
-            <Textarea id="rawdata" className="w-96 h-48 " />
+            <Textarea
+              id="rawdata"
+              className="w-96 h-48 "
+              onChange={(e) => setRawData(e.target.value)}
+            />
           </TabsContent>
           <TabsContent value="file">
             Select a file:
@@ -68,7 +108,12 @@ const ConnectDataDialog = ({ selected }: { selected: DirectoryTree }) => {
               ))}
           </TabsContent>
           <TabsContent value="import">
-            <Input type="file" id="import" className="col-span-3" />
+            <Input
+              type="file"
+              id="import"
+              className="col-span-3"
+              onChange={(e) => setImportedFile(e.target.files?.[0] || null)}
+            />
           </TabsContent>
           <TabsContent value="fetch">
             <div className="grid gap-4 py-4">
@@ -76,31 +121,57 @@ const ConnectDataDialog = ({ selected }: { selected: DirectoryTree }) => {
                 <Label htmlFor="url" className="text-right">
                   URL
                 </Label>
-                <Input id="url" className="col-span-3" />
+                <Input
+                  id="url"
+                  className="col-span-3"
+                  onChange={(e) =>
+                    setFetchData({ ...fetchData, url: e.target.value })
+                  }
+                />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="method" className="text-right">
                   Method
                 </Label>
-                <Input id="method" className="col-span-3" />
+                <Input
+                  id="method"
+                  className="col-span-3"
+                  onChange={(e) =>
+                    setFetchData({ ...fetchData, method: e.target.value })
+                  }
+                />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="headers" className="text-right">
                   Headers
                 </Label>
-                <Input id="headers" className="col-span-3" />
+                <Input
+                  id="headers"
+                  className="col-span-3"
+                  onChange={(e) =>
+                    setFetchData({ ...fetchData, headers: e.target.value })
+                  }
+                />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="body" className="text-right">
                   Body
                 </Label>
-                <Input id="body" className="col-span-3" />
+                <Input
+                  id="body"
+                  className="col-span-3"
+                  onChange={(e) =>
+                    setFetchData({ ...fetchData, body: e.target.value })
+                  }
+                />
               </div>
             </div>
           </TabsContent>
         </Tabs>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit" onClick={handleSubmit}>
+            Save changes
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
