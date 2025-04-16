b88f7677d30484a15b484ead7298afed001748e4
marcomit <marcomenegazzi.05@gmail.com>
2025-04-16 15:07:06 +0200

    stili e classi sulle tabelle

--- server/templates/test_test/index.js
+++ server/templates/test_test/index.js
@@ -4,26 +4,31 @@ import veloci from "@/syntax/components";
 export default async (content) => {
   const receipts = (await content.getScontriniData())["scontrini"];
 
-  return {
-    content: pdf.div(
-      pdf.h1("Report test").$("class", "text-center text-red-500"),
-      veloci.table(receipts, {
-        columns: [
-          { name: "test", value: "_id" },
-          {
-            name: "minimo",
-            value: veloci.min("righe", "prezzoUnitario"),
-          },
-          {
-            name: "massimo",
-            value: veloci.max("righe", "prezzoUnitario"),
-          },
-          {
-            name: "somma",
-            value: veloci.sum("righe", "prezzoUnitario"),
-          },
-        ],
-      })
-    ),
-  };
+  return pdf.div(
+    pdf.h1("Report test").$("class", "text-center text-red-500"),
+    veloci.table(receipts, {
+      columns: [
+        { name: "test", value: "_id" },
+        {
+          name: "minimo",
+          value: veloci.min("righe", "prezzoUnitario"),
+        },
+        {
+          name: "massimo",
+          value: veloci.max("righe", "prezzoUnitario"),
+        },
+        {
+          name: "righe",
+          value: 'righe.length',
+          variant: 'primary',
+          //class: "text-red-500"
+          class: (el, i) => i % 2 == 0 ? 'text-red-500' : 'text-green-500'
+        },
+        {
+          name: "somma",
+          value: veloci.sum("righe", "prezzoUnitario"),
+        },
+      ],
+    })
+  );
 };
